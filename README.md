ee99fd4a3a8a6b52741efb48523b966fc4be6452
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-03-01 15:36:51 -0800

    added a license

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Prints out a binary tree with formatting.
 ```
 $ java src/net/remasteredarch/binaryTreeFormatter/Formatter.java
 ```
+
+## License
+
+Binary Tree Formatter is licensed under the GNU General Public License version 3, or (at your option) any later version. You should have received a copy of the GNU General Public License along with Binary Tree Formatter, found in [LICENSE](./LICENSE). If not, see <[https://www.gnu.org/licenses/](https://www.gnu.org/licenses/)>.
--- src/main/java/net/remasteredarch/binaryTreeFormatter/Formatter.java
+++ src/main/java/net/remasteredarch/binaryTreeFormatter/Formatter.java
@@ -1,3 +1,18 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Copyright © 2024 RemasteredArch
+ *
+ * This file is part of Binary Tree Formatter.
+ *
+ * Binary Tree Formatter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Binary Tree Formatter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Binary Tree Formatter. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package net.remasteredarch.binaryTreeFormatter;
 
 import java.util.ArrayList;
